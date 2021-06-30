783f6af6a89257d64a14df66af1d39427eae3297
Rafaela <rafaelazaccarias@gmail.com>
2021-06-30 12:42:22 -0300

    Rotas de entrar e cadastrar

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 /* import { CommonModule, Routes } from '@angular/common'; */
 
 const routes: Routes = [
 
-  {path: ' ', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
   {path: 'entrar', component:  EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent}
 ];
 
 @NgModule({
-
    declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
